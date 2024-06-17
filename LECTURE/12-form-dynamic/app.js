905f564f9e8d7a0edf09a6a7c3be475c0af4770a
cmkoi1 <cmkoi1003@gmail.com>
2024-06-17 18:12:19 +0900

    study: json & 외부 API 활용

---
+++ LECTURE/12-form-dynamic/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const PORT = 9000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('dynamic',{title: '동적 폼을 배워보자'});
+})
+
+// ajax
+app.get('/ajax', (req, res) => {
+    // GET 방식이므로 브라우저에서 URL 쿼리까지 직접 입력해서도 값을 확인 가능
+    // http://localhost:9000/ajax?name=션&gender=남자
+    console.log(req.query);
+    res.send(req.query);
+})
+
+app.post('/ajax', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+
+// axios
+app.get('/axios', (req, res) => {
+    console.log(req.query);
+    res.send(req.query);
+})
+
+app.post('/axios', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+
+// fetch
+app.get('/fetch', (req, res) => {
+    console.log(req.query);
+    res.send(req.query);
+})
+
+app.post('/fetch', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+})
